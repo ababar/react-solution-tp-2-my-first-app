782c6cda4cb49ec47ea1bfddc1ba307ca7ed7d33
Noureddine ABABAR <noureddine.ababar@gmail.com>
2022-09-28 23:05:25 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Products from './Compsants/Products';
+
+const produits = [
+  {
+    id: 1,
+    title: 'PC Portable Gamer HP VICTUS',
+    price: '7490 DH',
+    thumbnail : 'HP16D0195NF.jpg'
+  },
+  {
+    id: 2,
+    title: 'PC Portable Gamer',
+    price: '2190 DH',
+    thumbnail : 'HP14424U3EA.jpg'
+  },
+  {
+    id: 3,
+    title: 'Pc Portable Chromebook Acer',
+    price: '3640 DH',
+    thumbnail: 'NXATHEF002.jpg'
+  },
+  {
+    id: 4,
+    title: 'PC Portable - HUAWEI',
+    price: '1270 DH',
+    thumbnail: 'HUA6901443442959.jpg'
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Products produits={produits} />
   );
 }
-
 export default App;
